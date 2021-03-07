b4c73f1c7d889e55b95fe7b9dadc79df1a0a173f
terahje <terahjequinn@gmail.com>
2021-03-06 17:32:20 -0700

    updated header and landing background

--- boo-wedding/src/App.js
+++ boo-wedding/src/App.js
@@ -1,23 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-function App() {
+class App extends Component {
+    render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-big-content demo-layout-transparent mdl-layout mdl-js-layout">
+        <Layout>
+            <Header className="mdl-layout__header mdl-layout__header--transparent" title=" " scroll>
+                <Navigation> 
+                    <Link to="/ourStory" className="nav-color">Our Story</Link>
+                    <Link to="/gallery" className="nav-color">Gallery</Link>
+                    <Link to="/festivities" className="nav-color">Festivities</Link>
+                    <Link to="/venue" className="nav-color">Venue</Link>
+                    <Link to="/registry" className="nav-color">Registry</Link>
+                    <Link to="/toDo" className="nav-color">To Do/ To Eat</Link>  
+                </Navigation>
+            </Header>
+            <Drawer title="">
+                <Navigation className="nav-color">
+                    <Link to="/ourStory" className="nav-color">Our Story</Link>
+                    <Link to="/gallery" className="nav-color">Gallery</Link>
+                    <Link to="/festivities" className="nav-color">Festivities</Link>
+                    <Link to="/venue" className="nav-color">Venue</Link>
+                    <Link to="/registry" className="nav-color">Registry</Link>
+                    <Link to="/toDo" className="nav-color">To Do/ To Eat</Link>  
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main></Main>
+            </Content>
+        </Layout>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
